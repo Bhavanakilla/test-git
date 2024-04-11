dae16252b24478d34882ad9cc22c1d82babb78e8
Bhavanakilla <165904767+Bhavanakilla@users.noreply.github.com>
2024-04-11 16:34:25 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,26 @@
+public class add{    
+	public static void main(String[] args){    
+		
+		//integer data type
+		int a=10;    
+		int b=10;    
+		int c=a+b;
+		
+		// float data type
+		float f=3.14f;
+		
+		// character data type
+		char ch='B';
+		//short data type
+		short sh=32000;
+		
+		// byte data type
+		 byte num = 100; 
+       
+		
+		
+		//double d=a;
+		double pi = 3.14159;
+		System.out.println(pi);
+	}
+}
